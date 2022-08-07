6d8b21609dca66ae5ba5cb13afbeef4c56b4b120
Duongntd <103487096+Duongntd@users.noreply.github.com>
2022-08-07 15:42:17 +0200

    add some style

--- src/routes/blitzsend.jsx
+++ src/routes/blitzsend.jsx
@@ -21,18 +21,33 @@ export default function Blitzsend() {
 	for (let input in inputData) {
 		if (input.slice(0, 7) === 'message') inputs.push(input);
 	}
-	const inputField = inputs.map((input) => {
+
+	const onFocus = (event) => {
+		event.target.placeholder = '';
+		event.target.previousElementSibling.classList.add('focusing');
+	};
+	const onBlur = (event, index) => {
+		event.target.placeholder = inputs[index].slice(7);
+		event.target.previousElementSibling.classList.remove('focusing');
+	};
+	const inputField = inputs.map((input, index) => {
 		return (
-			<div className='container input' key={input}>
-				<label htmlFor={input}>{input.slice(7)}</label>
+			<div className='input-container' key={input}>
+				<label className='input-label' htmlFor={input}>
+					{input.slice(7)}
+				</label>
 				<input
 					type={input === 'messagePassword' ? 'password' : 'text'}
 					id={input}
 					name={input}
+					className='input-box'
+					placeholder={input.slice(7)}
 					value={inputData.input}
 					onChange={(event) => {
 						setInputData({ ...inputData, [input]: event.target.value });
 					}}
+					onFocus={(event) => onFocus(event)}
+					onBlur={(event) => onBlur(event, index)}
 				/>
 			</div>
 		);
@@ -61,11 +76,24 @@ export default function Blitzsend() {
 			<header>
 				<h2>Blitzsend</h2>
 			</header>
-			<section className='form-input'>
-				<form onSubmit={(event) => handleSubmit(event)}>
+			<section className='form-section'>
+				<form
+					className='form-container'
+					onSubmit={(event) => handleSubmit(event)}>
+					<div className='file-type-sel'>
+						<span>Text</span>
+						<span>Image</span>
+						<span>File</span>
+					</div>
+					<div className='flex'>
+						<div className='input-container'>{inputField}</div>
+						<label htmlFor=''>
+							Set timer
+							<input type='text' />
+						</label>
+					</div>
 					<button>Submit</button>
 				</form>
-				{inputField}
 			</section>
 		</main>
 	);
